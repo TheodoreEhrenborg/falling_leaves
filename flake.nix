8d993d2f7c62912861089014dd7a83f8ffb8cd6a
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2024-05-05 22:41:22 +0100

    Install elm in nix-shell

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/807c549feabc";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils}:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        with pkgs;
+        {
+          devShells.default = mkShell {
+            packages = [
+			elmPackages.elm
+                          ];
+          };
+        }
+      );
+}
