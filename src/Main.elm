38a879136d1e1cea71b5a0078342b0a6a3cd0acd
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2024-05-05 22:56:22 +0100

    Use the playground package

--- src/Main.elm
+++ src/Main.elm
@@ -1,25 +1,11 @@
-module Main exposing (..)
-
-import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+module Main exposing (main)
+import Playground exposing (..)
 
 main =
-  Browser.sandbox { init = 0, update = update, view = view }
-
-type Msg = Increment | Decrement
-
-update msg model =
-  case msg of
-    Increment ->
-      model + 1
-
-    Decrement ->
-      model - 1
-
-view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
+  picture
+    [ triangle green 150
+    , circle white 40
+    , circle black 10
     ]
+
+-- See https://package.elm-lang.org/packages/evancz/elm-playground/latest/Playground
