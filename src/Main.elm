cb040a5213bb87e68e3cd8a43bd3a87b3a8a22c9
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2024-05-12 17:24:02 +0100

    Commit some changes---not sure what

--- src/Main.elm
+++ src/Main.elm
@@ -1,11 +1,27 @@
 module Main exposing (main)
-import Playground exposing (..)
+import Playground exposing (Time, triangle, green, moveX, moveY, rotate, game)
+import Time exposing (posixToMillis)
 
 main =
-  picture
-    [ triangle green 150
-    , circle white 40
-    , circle black 10
-    ]
+  game view update [Leaf 0 0, Leaf 0 100]
+
+view computer memory =
+  List.map (\leaf ->  triangle green 40
+      |> moveX leaf.x
+      |> moveY leaf.y
+      |> rotate 180
+   ) memory
+
+type alias Leaf =
+  { x : Float
+  , y : Float
+  }
+
+update computer memory =
+  memory ++ newLeaf computer.time
+
+newLeaf : Playground.Time -> List Int
+newLeaf foo =  foo --if time modBy 10 == 0 then [Leaf (Time.posixToMillis posix) 0] else []
+
 
 -- See https://package.elm-lang.org/packages/evancz/elm-playground/latest/Playground
